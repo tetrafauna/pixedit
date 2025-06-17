03b752b0f93abf55a566f6068770babc39c29e36
= <dice.fridge@mail.com>
2025-06-16 23:38:40 -0400

    I forgot what I Changed

--- build.sh
+++ build.sh
@@ -2,4 +2,14 @@
 
 SRC="main.c init.c input.c menu.c draw.c user_input.c"
 
-gcc -g $SRC -lSDL3 -lSDL3_image -o pixedit
+ld -r -b binary textures/alph.png -o alph.o
+ld -r -b binary textures/color_picker.png -o color_picker.o
+
+#gcc -g $SRC alph.o color_picker.o -lSDL3 -lSDL3_image -o pixedit
+
+#gcc -c src/*.c
+
+gcc -g ./src/*.c ./*.o -o pixedit -lSDL3 -lSDL3_image
+
+rm *.o
+
--- src/init.c
+++ src/init.c
@@ -18,11 +18,26 @@ extern SDL_Surface* color_select_surface;
 //draw.c
 extern SDL_Texture* tile_texture; 
 
+extern char _binary_textures_alph_png_start[];
+extern char _binary_textures_alph_png_end[];
+
+extern char _binary_textures_color_picker_png_start[];
+extern char _binary_textures_color_picker_png_end[];
+
+#define _alph_start  _binary_textures_alph_png_start
+#define _alph_end    _binary_textures_alph_png_end
+#define _alph_len    _alph_end-_alph_start 
+
+#define _color_picker_start  _binary_textures_color_picker_png_start
+#define _color_picker_end    _binary_textures_color_picker_png_end
+#define _color_picker_len    _color_picker_end-_color_picker_start 
+
 void init(){
 	if(!SDL_Init(SDL_INIT_VIDEO)) {
 		fprintf(stderr,"Failed to initalize SDL: %s\n",SDL_GetError());
 		exit(1);
 	}	
+
 	win_width=1280;
 	win_height=960;
 
@@ -31,6 +46,7 @@ void init(){
 		fprintf(stderr,"Failed to Create Window: %s\n",SDL_GetError()); 
 		exit(1);
 	}
+
 
 	rend = SDL_CreateRenderer(win,0);
 	if(!rend) {
@@ -41,8 +57,13 @@ void init(){
 	SDL_SetRenderDrawBlendMode(rend,SDL_BLENDMODE_BLEND);
 
 	//TODO embed this using ld
-	SDL_Surface* ap = IMG_Load("./textures/alph.png");
-	color_select_surface = IMG_Load("./textures/color_select.png");
+	
+	SDL_IOStream* cp_s = SDL_IOFromConstMem(_color_picker_start,_color_picker_len);
+	SDL_IOStream* a_s = SDL_IOFromConstMem(_alph_start,_alph_len);
+	
+	SDL_Surface* ap = IMG_LoadPNG_IO(a_s);
+	color_select_surface = IMG_LoadPNG_IO(cp_s);
+	
 	if(!color_select_surface) {
 		fprintf(stderr,"ERROR LOADING TEXTURE: %s",SDL_GetError());
 	}
